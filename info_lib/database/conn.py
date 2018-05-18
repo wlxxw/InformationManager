8a6fcf62f07746105c41f44cd430f3861d8b5c22
root <root@localhost.localdomain>
2018-05-18 16:16:44 +0800

    save

--- info_lib/database/conn.py
+++ info_lib/database/conn.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from InformationManager.global import base
+from InformationManager.info_lib.parse import base
 from sqlalchemy.exc import OperationalError
 
 BaseModel = declarative_base()
@@ -16,15 +16,18 @@ class Conn(object):
             self.engine.dispose()
 
     def initialize(self,
-                   pwd=base.ALA_MYSQL_ROOT_PASSWORD,
+                   pwd=base.MYS_PASSWD,
                    ip='localhost',
-                   port=base.ALA_MYSQL_PORT,
-                   database=base.DBCONFIG_DATABASE_NAME):
+                   port=base.MYS_PORT,
+                   database=base.MYS_BASIC_DATABASE):
         try:
-            conn_str = 'mysql://root:%s@%s:%s/%s?charset=utf8&unix_socket=%s' % (pwd, ip,
-                                                                                 port,
-                                                                                 database,
-                                                                                 base.ALA_MYSQL_UNIX_SOCKET)
+          #  conn_str = 'mysql://root:%s@%s:%s/%s?charset=utf8&unix_socket=%s' % (pwd, ip,
+          #                                                                       port,
+          #                                                                       database,
+          #                                                                       base.ALA_MYSQL_UNIX_SOCKET)
+            conn_str = 'mysql+pymysql://root:%s@%s:%s/%s?charset=utf8' % (pwd, ip,
+                                                                       port,
+                                                                       database)
             self.engine = create_engine(
                 conn_str,
                 pool_recycle=3600,
@@ -53,5 +56,4 @@ class Conn(object):
 
 
 if __name__ == "__main__":
-    conn = Conn().initialize()
-    print conn[0]
+    conn = Conn()
